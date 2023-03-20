91d6bb9cd6f9525c58ee9a29d10a1abf96d53283
Lucas Lancia <125708468+LucasLancia@users.noreply.github.com>
2023-03-20 16:55:58 -0300

    Update models.py

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -10,7 +10,7 @@ class Curso(models.Model):
 class Estudiante(models.Model):
     nombre=models.CharField(max_length=40)
     apellido=models.CharField(max_length=40)
-    telefono=models.CharField(max_length=20)
+    ##telefono=models.CharField(max_length=20)
     email=models.EmailField()
 
     def __str__(self):
@@ -19,9 +19,9 @@ class Estudiante(models.Model):
 class Profesor(models.Model):
     nombre=models.CharField(max_length=40)
     apellido=models.CharField(max_length=40)
-    telefono=models.CharField(max_length=20)
+    ##telefono=models.CharField(max_length=20)
     email=models.EmailField()
     profesion= models.CharField(max_length=40)
 
     def __str__(self):
-        return f"Profesor: {self.nombre},{self.apellido},telefono:{self.telefono},email:{self.email},profesion:{self.profesion}"
+        return f"Profesor: {self.nombre},{self.apellido},email:{self.email},profesion:{self.profesion}"
